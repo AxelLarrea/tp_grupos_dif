20b599432fdd71fffabf3b7706d91b938cc1297c
Axel Larrea <axel.larrea.9@gmail.com>
2022-06-08 08:30:55 -0300

    yep

---
+++ src/components/Canal.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+
+
+const Canal = ({nombre}) => {
+
+    const [activos, setActivos] = useState()
+
+    const cantidad_activos = async () => {
+        const response = await fetch(`http://localhost:4000/activoscanal/${nombre}`);
+        const data = await response.json();
+        setActivos(data[1]);
+    };
+
+    cantidad_activos();
+
+    const handleClick = async () => {
+
+        await fetch(`http://localhost:4000/desubscribir/${nombre}`, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"}
+        });
+        window.location.href = '/';
+    }
+
+    return (
+        <div className="channel-container">
+            <p className="channel-name">{nombre}</p>
+            <p className="activos">Activos: {activos}</p>
+            <input className="btn-name" type="button" value="Desubscribir" onClick={handleClick} />
+        </div>
+    );
+}
+ 
+export default Canal;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import Canales from '../components/Canales';
+
+const Home = () => {
+    return (
+        <>
+            <Header/>
+            <Canales/>
+        </>
+    );
+}
+ 
+export default Home;
